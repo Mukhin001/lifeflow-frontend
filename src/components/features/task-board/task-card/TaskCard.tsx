e43c6872baa17e644e7d1b81f6098b8b7ed0ff2e
Mukhin001 <purgen.igor@yandex.ru>
2026-06-05 14:56:31 +0300

    feat(task-board): убрали лишнии шрифты в файле layout

--- src/components/features/task-board/task-card/TaskCard.tsx
+++ src/components/features/task-board/task-card/TaskCard.tsx
@@ -2,7 +2,6 @@ import { Task, UpdateTaskDto } from "@/src/api/task/task.types";
 import { useState } from "react";
 import TaskView from "./TaskView";
 import TaskEditForm from "./TaskEditForm";
-import style from "./css/task-card.module.css";
 
 type Props = {
   task: Task;
